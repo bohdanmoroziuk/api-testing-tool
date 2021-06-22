d1a6a45cb000dc0d83b6fe1ef777cead667e2119
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-06-22 14:43:12 +0300

    feat: send requests

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+const form = document.querySelector('[data-form]');
+
+const methodSelect = document.querySelector('[data-method]');
+const urlInput = document.querySelector('[data-url]');
 
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
@@ -8,6 +14,19 @@ const addQueryParamButton = document.querySelector('[data-add-query-param-btn]')
 const addRequestHeaderButton = document.querySelector('[data-add-request-header-btn]');
 
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
+
+const keyValuePairsToObject = (container) => {
+  const pairs = container.querySelectorAll('[data-key-value-pair]');
+
+  return [...pairs].reduce((data, pair) => {
+    const key = pair.querySelector('[data-key]').value;
+    const value = pair.querySelector('[data-value]').value;
+
+    if (key === '') return data;
+
+    return { ...data, [key]: value };
+  }, {})
+};
 
 const handleQueryParamAdd = () => {
   queryParamsContainer.append(createKeyValuePair());
@@ -31,8 +50,23 @@ const createKeyValuePair = () => {
   return element;
 };
 
+const handleRequestSend = (event) => {
+  event.preventDefault();
+
+  axios({
+    url: urlInput.value,
+    method: methodSelect.value,
+    params: keyValuePairsToObject(queryParamsContainer),
+    headers: keyValuePairsToObject(requestHeadersContainer),
+  })
+    .then(response => console.log(response.data))
+    .catch(console.error);
+};
+
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
 addQueryParamButton.addEventListener('click', handleQueryParamAdd);
 addRequestHeaderButton.addEventListener('click', handleRequestHeaderAdd);
+
+form.addEventListener('submit', handleRequestSend);
